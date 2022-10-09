1b77df0e684df0e33b355add04dedd791500699e
James <james.yao1@ntlworld.com>
2022-10-09 17:34:01 +0100

    Now displaying the subreddit and a link to the original in ugly colors ;)

--- js/index.js
+++ js/index.js
@@ -7,10 +7,36 @@ async function scrape(url) {
 async function main() {
   const redditData = (await scrape("https://www.reddit.com/.json")).data
     .children;
-  console.log(redditData);
+
+  const mainDisplay = document.getElementById("main_display");
+
+  console.log(redditData[0].data);
 
   for (let i = 0; i < redditData.length; i++) {
-    console.log(redditData[i]);
+    const postDiv = document.createElement("div");
+    postDiv.className = "post";
+
+    const subredditDiv = document.createElement("div");
+    subredditDiv.className = "subreddit";
+    subredditDiv.innerHTML = redditData[i].data.subreddit;
+
+    // Use the image preview
+    //const preview = document.createElement("img");
+    // preview.src = redditData[i].data.preview?.images[0]?.source.url;
+    // Sadly it seems getting the preview is 403 forbidden :'()
+    // preview.alt = "Preview of this post";
+
+    // Create a link to the original post
+    const link = document.createElement("a");
+    link.className = "post_link";
+    link.textContent = "Take me to the post!";
+    link.href = "https://www.reddit.com" + redditData[i].data.permalink;
+
+    // Add to main div
+    //postDiv.appendChild(preview);
+    postDiv.appendChild(subredditDiv);
+    postDiv.appendChild(link);
+    mainDisplay.appendChild(postDiv);
   }
 }
 
