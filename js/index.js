4264a9ae28cef58e56bbdb8bd6009d7548ba9028
James <james.yao1@ntlworld.com>
2022-10-09 17:52:39 +0100

    Added a thumbnail for posts

--- js/index.js
+++ js/index.js
@@ -10,30 +10,40 @@ async function main() {
 
   const mainDisplay = document.getElementById("main_display");
 
-  console.log(redditData[0].data);
+  console.log(redditData);
 
   for (let i = 0; i < redditData.length; i++) {
+    // The useful data
+    const postInfo = redditData[i].data;
+    // Create a container div
     const postDiv = document.createElement("div");
     postDiv.className = "post";
 
+    // Get the subreddit name
     const subredditDiv = document.createElement("div");
     subredditDiv.className = "subreddit";
-    subredditDiv.innerHTML = redditData[i].data.subreddit;
+    subredditDiv.innerHTML = postInfo.subreddit;
 
-    // Use the image preview
-    //const preview = document.createElement("img");
-    // preview.src = redditData[i].data.preview?.images[0]?.source.url;
-    // Sadly it seems getting the preview is 403 forbidden :'()
-    // preview.alt = "Preview of this post";
+    // Get the post title
+    const postTitle = document.createElement("div");
+    postTitle.className = "post_title";
+    postTitle.innerHTML = postInfo.title;
+
+    // Use the thumbnail
+    const thumbnail = document.createElement("img");
+    thumbnail.src = redditData[i].data.thumbnail;
+    thumbnail.className = "thumbnail";
+    thumbnail.alt = "Thumbnail";
 
     // Create a link to the original post
     const link = document.createElement("a");
     link.className = "post_link";
     link.textContent = "Take me to the post!";
-    link.href = "https://www.reddit.com" + redditData[i].data.permalink;
+    link.href = "https://www.reddit.com" + postInfo.permalink;
 
     // Add to main div
-    //postDiv.appendChild(preview);
+    postDiv.appendChild(postTitle);
+    postDiv.appendChild(thumbnail);
     postDiv.appendChild(subredditDiv);
     postDiv.appendChild(link);
     mainDisplay.appendChild(postDiv);
