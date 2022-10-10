6e6823bfaa97413ff78e02e3330254a2404ea342
James <james.yao1@ntlworld.com>
2022-10-10 22:24:32 +0100

    minor styling improvements

--- js/index.js
+++ js/index.js
@@ -15,14 +15,23 @@ async function main() {
   for (let i = 0; i < redditData.length; i++) {
     // The useful data
     const postInfo = redditData[i].data;
-    // Create a container div
-    const postDiv = document.createElement("div");
+
+    // Create a container div which also links to the original
+    const postDiv = document.createElement("a");
     postDiv.className = "post";
+    postDiv.href = "https://www.reddit.com" + postInfo.permalink;
 
     // Get the subreddit name
     const subredditDiv = document.createElement("div");
     subredditDiv.className = "subreddit";
-    subredditDiv.innerHTML = postInfo.subreddit;
+    // Add subreddit with some styling
+    const subR = document.createElement("div");
+    const subRname = document.createElement("div");
+    subR.innerHTML = "r/";
+    subRname.innerHTML = postInfo.subreddit;
+    subR.className = "r";
+    subredditDiv.appendChild(subR);
+    subredditDiv.appendChild(subRname);
 
     // Get the post title
     const postTitle = document.createElement("div");
@@ -35,17 +44,10 @@ async function main() {
     thumbnail.className = "thumbnail";
     thumbnail.alt = "Thumbnail";
 
-    // Create a link to the original post
-    const link = document.createElement("a");
-    link.className = "post_link";
-    link.textContent = "Take me to the post!";
-    link.href = "https://www.reddit.com" + postInfo.permalink;
-
     // Add to main div
     postDiv.appendChild(postTitle);
     postDiv.appendChild(thumbnail);
     postDiv.appendChild(subredditDiv);
-    postDiv.appendChild(link);
     mainDisplay.appendChild(postDiv);
   }
 }
